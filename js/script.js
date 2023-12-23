5c8297bc0a907bcf74bb765d526ec784b6909254
Khairunas R W <128572429+khairunas001@users.noreply.github.com>
2023-12-24 00:07:24 +0700

    anjay jadi

--- js/script.js
+++ js/script.js
@@ -3,14 +3,22 @@
 let BtnIncome = document.getElementById('btnIncome');
 let BtnExpanse = document.getElementById('btnExpanse');
 
-//tBody table
-let tBodyInc = document.getElementById('tBody-inc');
-let  tBodyExp = document.getElementById('tBody-exp');
-
 //Global Variable
 let transaction = {
-    income: [],
-    expanse:[]
+    income: [
+        {
+            id: 1,
+            transaksi:'salary',
+            saldo: 5000000
+        }
+    ],
+    expanse:[
+        {
+            id: 1,
+            transaksi:'Bayar Tagihan',
+            saldo: 50000
+        }
+    ]
 }
 
 function incomeHandler(){
@@ -29,6 +37,8 @@ function incomeHandler(){
         saldo:saldoValue
     }
     transaction.income.push(tempObj);
+    getTransaction();
+    saldoCount();
     // console.log('Income');
     // console.log(transaction);
     alert(`${namaValue} telah ditambahkan`);
@@ -50,13 +60,54 @@ function ExpanseHandler(){
         saldo:saldoValue
     }
     transaction.expanse.push(tempObj);
+    getTransaction();
+    saldoCount();
     // console.log('expanse');
     // console.log(transaction);
     alert(`${namaValue} telah ditambahkan`);
 }
 
+function getTransaction(){
+    //tBody table
+    let tBodyInc = document.getElementById('tBody-inc');
+    let  tBodyExp = document.getElementById('tBody-exp');
+    tBodyInc.innerHTML='';
+    tBodyExp.innerHTML='';
+    transaction.income.forEach(inc =>{
+        tBodyInc.innerHTML += `<tr>
+        <td>${inc.id}</td>
+        <td>${inc.transaksi}</td>
+        <td>${inc.saldo}</td>
+        </tr>`
+    })
+    transaction.expanse.forEach(exp =>{
+        tBodyExp.innerHTML += `<tr>
+        <td>${exp.id}</td>
+        <td>${exp.transaksi}</td>
+        <td>${exp.saldo}</td>
+        </tr>`
+    })
+}
+
+function saldoCount(){
+    let saldoIncome = 0;
+    let saldoExpanse = 0;
+    transaction.income.forEach (inc =>{
+        saldoIncome += +inc.saldo
+    })
+    transaction.expanse.forEach (exp =>{
+        saldoExpanse += +exp.saldo
+    })
+    let selisih = saldoIncome-saldoExpanse;
+    
+    let saldoValue = document.getElementById('saldo-value')
+    saldoValue.innerHTML = selisih;
+    // console.log(selisih);
+}
 
 
 // Add Event Listener
 BtnIncome.addEventListener('click', incomeHandler);
 BtnExpanse.addEventListener('click', ExpanseHandler);
+getTransaction();
+saldoCount();
